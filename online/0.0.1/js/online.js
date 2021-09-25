464ea34dc4cfb80be0e297073e22ca818f8c72ef
jcguanqi <jcguanqi@126.com>
2021-09-25 15:56:26 +0800

    fix online bug

--- online/0.0.1/js/online.js
+++ online/0.0.1/js/online.js
@@ -2,7 +2,7 @@
  * @Author: fegq
  * @Date: 2021-09-25 12:06:33
  * @LastEditors: fegq
- * @LastEditTime: 2021-09-25 12:07:39
+ * @LastEditTime: 2021-09-25 15:56:06
  * @Description: This is a check online status!
  * @Version: 0.0.1
  */
@@ -18,7 +18,7 @@ window.addEventListener('offline', function (e) {
 function handleNetWork(status) {
     let onlineDiv = document.createElement('div');
     onlineDiv.className = 'online';
-    onlineDiv.innerHTML = '<p><i class="fa fa-wifi"></i> 对不起，你的网络已断开，请点击<a href="">重新刷新</a>试试看！</p>';
+    onlineDiv.innerHTML = '<p><i class="fa fa-wifi"></i>网络连接不可用</p>';
     let bodyFirst = document.body.firstChild;
     let onlineElem = document.querySelector('.online');
     if (onlineElem == null) {
