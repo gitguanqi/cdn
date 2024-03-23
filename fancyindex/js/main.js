b78ab0f1309bbd8380dadbb7bb15b7b7bc9458ee
mark <jcguanqi@126.com>
2024-03-23 15:37:26 +0800

    fix fancyindex

--- fancyindex/js/main.js
+++ fancyindex/js/main.js
@@ -45,20 +45,3 @@ jQuery.each(jQuery('#nginx-fancyindex-directory').text().split('/'), function(id
 });
 
 year.innerText = new Date().getFullYear();
-
-// 统计服务
-// <!-- Matomo -->
-/* <script type="text/javascript"> */
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//am.guanqi.xyz/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '15']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-// </script>
-// <!-- End Matomo Code -->
